d61cb1ecef7c882156f1583922e9e48ad53505bb
scrge <56807509+scrge-ai@users.noreply.github.com>
2022-03-29 17:15:51 +0000

    deleted redundant files, renamed define_bot.js and script.js

--- input_listener.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("keydown", function(a){
-	//console.log("key down")
-	keydown = true;
-});
-
-document.addEventListener("keyup", function(a){
-	//console.log("key up")
-	keydown = false;
-});
